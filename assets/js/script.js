b37f0be655f1499b23b87b578fdeb2d62d4c4c45
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-07-10 13:09:36 -0600

    questions

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+// Variables
+
+    // Game Elements
+    var Clock;
+    var Score; 
+
+    var StartScreen
+    var EndScreen
+    
+    var isCorrect;
+    var True
+    var False
+
+    // Questions
+    var questionList = [
+        {question:"What is spidermans name?",
+            answers: [
+            {answer: "Peter Parker",
+            isCorrect: True
+            },
+            {answer: "batman",
+            isCorrect: False
+            },
+            {answer: "bruce banner",
+            isCorrect: False
+            },
+            {answer: "dog",
+            isCorrect: False
+            }
+            ]
+        },
+        {question:"What color is the sky?",
+            answers: [
+            {answer: "blue",
+            isCorrect: True
+            },
+            {answer: "red",
+            isCorrect: False
+            },
+            {answer: "green",
+            isCorrect: False
+            },
+            {answer: "black",
+            isCorrect: False
+            }
+            ]
+        },
+        {question:"Which animal is a common house pet?",
+            answers: [
+            {answer: "Dog",
+            isCorrect: True
+            },
+            {answer: "Giraffe",
+            isCorrect: False
+            },
+            {answer: "Whale",
+            isCorrect: False
+            },
+            {answer: "Big Bird",
+            isCorrect: False
+            }
+            ]
+        },
+        {question:"Which item is food?",
+        answers: [
+            {answer: "Spaghetti",
+            isCorrect: True
+            },
+            {answer: "Nails",
+            isCorrect: False
+            },
+            {answer: "Purple",
+            isCorrect: False
+            },
+            {answer: "black",
+            isCorrect: False
+            }
+            ]
+        },  
+    ]
