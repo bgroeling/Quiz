7f25c74941c53b6404b33b364695784f0ab6923b
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-07-10 21:44:58 -0600

    Basic Structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
     // Game Elements
     var Clock;
     var Score; 
+    var ScoreBoard;
 
     var StartScreen
     var EndScreen
@@ -87,7 +88,31 @@
 
 // Functions
 
-    // Open Quiz Modal When Start Button is Clicked
-    StartButton.onclick = function() {
-        QuizModal.style.display = "block";
-    }
+    // Modal Screens
+
+        // Open Quiz Modal When Start Button is Clicked
+        StartButton.onclick = function() {
+            QuizModal.style.display = "block";
+        }
+
+        // Open False Screen Modal When False is clicked
+
+            // Go Back to Quiz Screen When "Next Question" is Clicked
+
+        // Open True Screen Modal When True is clicked
+
+            // Go Back to Quiz Screen When "Next Question" is Clicked
+
+    // Clock
+        
+        // When Start Button is Clicked, Begin Countdown
+
+        // When Clock = 0 The End Screen is opened
+
+    // Score
+        
+        // When True is Clicked, at 25 points to ScoreBoard
+
+        // When End Screen is active, display Score on Final ScoreBoard
+
+
