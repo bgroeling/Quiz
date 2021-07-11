105c10d2427a5ef10f233bba43b0569c45deb163
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-07-11 03:02:09 -0600

    creating functions
    creating functions to link the arrays with the display box in the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,97 @@
-// Variables
+// Start button in html = Start button in javascript
+const startButton = document.getElementById('StartButton');
+const questionContainerElement = document.getElementById('Quiz');
+setNextQuestion();
 
-    // Game Elements
-    var Clock;
-    var Score; 
-    var ScoreBoard;
+startButton.addEventListener('click', startQuiz);
 
-    var StartScreen
-    var EndScreen
-    
-    var isCorrect;
-    var True
-    var False
+function startQuiz() {
+    console.log('Quiz Started');
+    shuffleQuestions = questions.sort(() => Math.random() -.5);
+    currentQuestionIndex = 0
+    setNextQuestion();
+}
 
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+function selectAnswer() {
+
+}
+const QuestionList = [
+    {Question:"What is spidermans name?",
+        answers: [
+        {answer: "Peter Parker",
+        isCorrect: True
+        },
+        {answer: "batman",
+        isCorrect: False
+        },
+        {answer: "bruce banner",
+        isCorrect: False
+        },
+        {answer: "dog",
+        isCorrect: False
+        }
+        ]
+    },
+    {Question:"What color is the sky?",
+        answers: [
+        {answer: "blue",
+        isCorrect: True
+        },
+        {answer: "red",
+        isCorrect: False
+        },
+        {answer: "green",
+        isCorrect: False
+        },
+        {answer: "black",
+        isCorrect: False
+        }
+        ]
+    },
+    {Question:"Which animal is a common house pet?",
+        answers: [
+        {answer: "Dog",
+        isCorrect: True
+        },
+        {answer: "Giraffe",
+        isCorrect: False
+        },
+        {answer: "Whale",
+        isCorrect: False
+        },
+        {answer: "Big Bird",
+        isCorrect: False
+        }
+        ]
+    },
+    {Question:"Which item is food?",
+    answers: [
+        {answer: "Spaghetti",
+        isCorrect: True
+        },
+        {answer: "Nails",
+        isCorrect: False
+        },
+        {answer: "Purple",
+        isCorrect: False
+        },
+        {answer: "black",
+        isCorrect: False
+        }
+        ]
+    },  
+]
     // Modals
 
         // Quiz Modal
         var QuizModal = document.getElementById("Quiz-Modal");
-        var StartButton = document.getElementById("Start-Button");
+        var StartButton = document.getElementById("StartButton");
 
     // Questions
-    var questionList = [
-        {question:"What is spidermans name?",
-            answers: [
-            {answer: "Peter Parker",
-            isCorrect: True
-            },
-            {answer: "batman",
-            isCorrect: False
-            },
-            {answer: "bruce banner",
-            isCorrect: False
-            },
-            {answer: "dog",
-            isCorrect: False
-            }
-            ]
-        },
-        {question:"What color is the sky?",
-            answers: [
-            {answer: "blue",
-            isCorrect: True
-            },
-            {answer: "red",
-            isCorrect: False
-            },
-            {answer: "green",
-            isCorrect: False
-            },
-            {answer: "black",
-            isCorrect: False
-            }
-            ]
-        },
-        {question:"Which animal is a common house pet?",
-            answers: [
-            {answer: "Dog",
-            isCorrect: True
-            },
-            {answer: "Giraffe",
-            isCorrect: False
-            },
-            {answer: "Whale",
-            isCorrect: False
-            },
-            {answer: "Big Bird",
-            isCorrect: False
-            }
-            ]
-        },
-        {question:"Which item is food?",
-        answers: [
-            {answer: "Spaghetti",
-            isCorrect: True
-            },
-            {answer: "Nails",
-            isCorrect: False
-            },
-            {answer: "Purple",
-            isCorrect: False
-            },
-            {answer: "black",
-            isCorrect: False
-            }
-            ]
-        },  
-    ]
+    var 
 
 // Functions
 
@@ -93,10 +100,9 @@
         // Open Quiz Modal When Start Button is Clicked
         StartButton.onclick = function() {
             QuizModal.style.display = "block";
-        }
-
+        };
         // Open False Screen Modal When False is clicked
-
+        
             // Go Back to Quiz Screen When "Next Question" is Clicked
 
         // Open True Screen Modal When True is clicked
@@ -114,5 +120,10 @@
         // When True is Clicked, at 25 points to ScoreBoard
 
         // When End Screen is active, display Score on Final ScoreBoard
+
+    // Quiz 
+// 
+
+
 
 
